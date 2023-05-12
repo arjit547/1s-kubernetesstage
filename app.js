05b8aa3696e7c816f8cc1344246beb0c6031c0fd
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-05-12 09:11:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('KUBERNETES -+- The deployment, scaling, and maintenance of containerized applications are all automated via the open-source container orchestration technology known as Kubernetes.');
+  res.end('KUBERNETES +++ The deployment, scaling, and maintenance of containerized applications are all automated via the open-source container orchestration technology known as Kubernetes.');
 });
 
 server.listen(3000, () => {
